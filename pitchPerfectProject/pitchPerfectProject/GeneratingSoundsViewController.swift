52355b0193451a5363b6db66299086d8a89a2980
surnan <forsuresh@yandex.com>
2017-11-07 05:48:42 -0500

    View Controllers created and navigation connecting them has been setup

--- pitchPerfectProject/pitchPerfectProject/GeneratingSoundsViewController.swift
+++ pitchPerfectProject/pitchPerfectProject/GeneratingSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GeneratingSoundsViewController.swift
 //  pitchPerfectProject
 //
 //  Created by admin on 11/7/17.
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GeneratingSoundsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var test2: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        test2.text = "Hi"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
---
+++ pitchPerfectProject/pitchPerfectProject/RecordingSoundsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  pitchPerfectProject
+//
+//  Created by admin on 11/7/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+
+class RecordingViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var test: UILabel!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        test.text = "Hello World"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
